9c7bc975ccd7aa8df8c9298f1e18d6d9d013c9cc
AlaaYehia <alasyehia5@gmail.com>
2025-03-19 23:36:56 +0200

    first

---
+++ src/main/java/com/tain/TainApplication.java
@@ -0,0 +1,13 @@
+package com.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TainApplication.class, args);
+	}
+
+}
